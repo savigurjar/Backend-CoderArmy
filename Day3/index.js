c45cdb89531585f244ed9fd2a9031b291df3cb16
savigurjar <savigurjar1201@gmail.com>
2025-06-18 09:59:44 +0530

    libuv

--- Day3/index.js
+++ Day3/index.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
 setTimeout(() => {
   console.log("konnichiwa");
 }, 3000);
+
+// v8-engine -> libuv -> os
+
+fs.readFile("./data.json", (err, res) => {
+  console.log(res);
+});
