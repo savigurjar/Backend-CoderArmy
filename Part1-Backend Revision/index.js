f4e5dfc1b5ec5a78063a9b15ad654369ed59b00e
savigurjar <savigurjar1201@gmail.com>
2025-08-02 15:38:21 +0530

    lecture 5 revision compelete

--- Part1-Backend Revision/index.js
+++ Part1-Backend Revision/index.js
@@ -1,2 +1,11 @@
-const sum = require("./second");
-sum(6,7);
+// const sum = require("./second");
+// sum(6,7);
+
+// const http = require("http");
+import http from 'http';
+const server = http.createServer((req, res) => {
+  res.end("coderArmy");
+});
+server.listen(4000, () => {
+  console.log("I'm listen ");
+});
