3a041f5c8440b774fb31cb7845346c210d23dbaa
savigurjar <savigurjar1201@gmail.com>
2025-08-03 16:37:45 +0530

    middleware

--- Part1-Backend Revision/post.js
+++ Part1-Backend Revision/post.js
@@ -21,8 +21,26 @@ app.post("/food", (req, res) => {
   restaurant.push(req.body);
   res.send("Data saved Successfully");
 });
-
-
+app.patch("/food", (req, res) => {
+  console.log(req.body);
+  const Food = restaurant.find((info) => (info.id = req.body.id));
+  if (req.body.foodname) {
+    Food.foodname = req.body.foodname;
+  }
+  res.send("patch updated");
+});
+app.put("/food", (req, res) => {
+  const Food = restaurant.find((info) => info.id === req.body.id);
+  Food.foodname = req.body.foodname;
+  Food.price = req.body.price;
+  res.send("put updated successfully");
+});
+app.delete("/food/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = restaurant.findIndex((info) => info.id === id);
+  restaurant.splice(index, 1);
+  res.send("delete successfully");
+});
 app.listen(5000, (req, res) => {
   console.log("listening at 5000");
 });
