37d63d52713c855ce7014c830a7f2541f4d536a3
savigurjar <savigurjar1201@gmail.com>
2025-08-24 12:59:01 +0530

    socket id

--- Day32-WebSocket/index.js
+++ Day32-WebSocket/index.js
@@ -4,17 +4,38 @@ const { Server } = require("socket.io");
 const http = require("http");
 const server = http.createServer(app);
 const io = new Server(server);
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 io.on("connection", (socket) => {
-  socket.on("messgae", (data) => {
-    io.emit("new-msg", data);
+  console.log("A user connected:", socket.id);
+
+
+  
+  // create room//join room
+  socket.on("join-room", (room) => {
+    socket.join(room);
   });
-  socket.on("disconnected", () => {
-    console.log("disconnected from server"); 
+  // socket.on("message", (data) => {
+  //   console.log("Message received:", data);
+  //   // io.emit("new-message", data); // broadcast to all
+  //   socket.broadcast.emit("new-message", data);
+  // });
+
+  socket.on("message", ({ room, msg }) => {
+    // console.log("Message received:", data);
+    socket.to(room).emit("new-message", msg);
+  });
+
+  //  disconnect event
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
 server.listen(3000, () => {
   console.log("listening at port 3000");
 });
-// upgrade krke websocket pr le jayege
